10e34e31983f38b3745dc996849428251f9c634b
muhammed <muhammedmamu2906@gmail.com>
2024-11-21 19:17:59 +0530

    configuration completed

--- ERP/settings.py
+++ ERP/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(f%ae-2e#6pxsed_ja3*gi*dy97di#kh*6-(-ikiv4cszi@%ns"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Backend.onrender.com']
 
 
 # Application definition
